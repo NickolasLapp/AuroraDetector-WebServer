89af95dd392c2cc3a0e7e080bce0680b1206d045
Tyler Huffman <tyler.huffmanmt@gmail.com>
2011-06-24 13:16:42 -0600

    Fixed syntax errors, began working on parsing script.

--- dataParsing.php
+++ dataParsing.php
@@ -1,19 +1,32 @@
 <?php
-include_once('dataPaths.inc');
+$DATA_DIR = '//ORSL-MAXWELL/Data--Auxiliary/Aurora_Network/';
 $month = date('m');
 $day = date('d');
 $year = date('Y');
 
 function getRecentAuroraData($auroraData, $detectorID)
 {
-	$file = $DATA_DIR."/$detectorID/".date('Y-m-d', mktime(0,0,0,$month,$day,$year)).".txt";
+	$file = '//ORSL-MAXWELL/Data--Auxiliary/Aurora_Network/'.$detectorID.'/'."delete".'.txt';
 	if( file_exists($file) ){
     	$handle = fopen($file,"r");
-    	while($data = fgetcsv($handle, 1024)){}
+    	while($data = fgetcsv($handle, 1024, ',')){}
 		if ($auroraData == "PMT")
 			return $data[6];
 		else if ($auroraData == "PD")
 			return $data[7];
+	}
+	else
+		return $file;
+}
+
+function getEntireAuroraData($detectorID) {
+	$file = '//ORSL-MAXWELL/Data--Auxiliary/Aurora_Network/'.$detectorID.'/'."delete".'.txt';
+	if( file_exists($file) ){
+		$handle = fopen($file,"r");
+		while($data = fgetcsv($handle, 1024, ',')){
+			$array[] = $data[6];
+		}
+		return $array;
 	}
 }
 
