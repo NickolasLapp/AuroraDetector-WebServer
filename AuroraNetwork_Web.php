716ffd6b3f5701bf63d4a3b59a6488e8c5a474e7
Tyler Huffman <tyler.huffmanmt@gmail.com>
2011-08-16 13:42:06 -0600

    Made some changes to the AuroraNetwork_Web to allow for more accurate, current readings.

--- AuroraNetwork_Web.php
+++ AuroraNetwork_Web.php
@@ -102,17 +102,14 @@
 		$PMTData = getRecentPMTAuroraData($detectors['IDs'][$ii]);
 		$PDData	 = getRecentPDAuroraData($detectors['IDs'][$ii]);	
 		if ($PMTData == 'NaN' || $PDData == 'NaN') {
-			$descriptionText = $descriptionText.'Current Status: Standby';
-			$descriptionText = $descriptionText.'<br>PMT:<rad style = "text-align: left; margin-left: 10px;"> '.'#####'.' &#956;<i>W</i>/<i>m</i><sup>2</sup><i>Sr</i></rad>';
-			$descriptionText = $descriptionText.'<br>PD:<rad style = "text-align: left; margin-left: 15px;"> '.'#####'.' &#956;<i>W</i>/<i>m</i><sup>2</sup><i>Sr</i></rad>';
+			$descriptionText = $descriptionText.'Detector Status: Standby';
+			$descriptionText = $descriptionText.'<br>Light Status:<rad style = "text-align: left; margin-left: 10px;"> '.'Sunlight'.'</rad>';
 		}
 		else if ($PMTData == '' || $PDData == '') {
-			$descriptionText = $descriptionText.'Current Status: Inactive';
-			$descriptionText = $descriptionText.'<br>PMT:<rad style = "text-align: left; margin-left: 10px;"> '.'#####'.' &#956;<i>W</i>/<i>m</i><sup>2</sup><i>Sr</i></rad>';
-			$descriptionText = $descriptionText.'<br>PD:<rad style = "text-align: left; margin-left: 15px;"> '.'#####'.' &#956;<i>W</i>/<i>m</i><sup>2</sup><i>Sr</i></rad>';
+			$descriptionText = $descriptionText.'Detector Status: Inactive';
 		}
 		else {
-			$descriptionText = $descriptionText.'Current Status: On';
+			$descriptionText = $descriptionText.'Detector Status: On';
 			$descriptionText = $descriptionText.'<br>PMT:<rad style = "text-align: left; margin-left: 10px;"> '.$PMTData.' &#956;<i>W</i>/<i>m</i><sup>2</sup><i>Sr</i></rad>';
 			$descriptionText = $descriptionText.'<br>PD:<rad style = "text-align: left; margin-left: 15px;"> '.$PDData.' &#956;<i>W</i>/<i>m</i><sup>2</sup><i>Sr</i></rad>';
 		}
