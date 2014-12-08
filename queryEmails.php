9134aff1b440e99e3762270f26c497ba5c377f5a
nick.lapp@foundant.com <nick.lapp@foundant.com>
2014-12-07 22:10:23 -0700

    Adding Authentification On Query Email Page

--- queryEmails.php
+++ queryEmails.php
@@ -1,5 +1,9 @@
 <?php
 require_once("./include/membersite_config.php");
 
-echo $fgmembersite->ReturnSystemUsers();
+if($fgmembersite->CheckLogin() && $fgmembersite->UserFullName() == 'AuroraUser' && $fgmembersite->UserEmail() == 'aurora.montana@gmail.com')
+	echo $fgmembersite->ReturnSystemUsers();
+else
+	echo "Access Denied";
+
 ?>
